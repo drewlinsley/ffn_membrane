Fixed path saving and normalization for ffns.

ffn/inference/inference.py
@@ -1128,8 +1128,12 @@ class Runner(object):
       logging.info('Could not match histogram: %r', e)
       return None, None
 
-    image = (image.astype(np.float32) -
-             self.request.image_mean) / self.request.image_stddev
+    image = (image.astype(np.float32) - self.request.image_mean) / self.request.image_stddev
+    # # JK did not separate membranes and images
+    # im = image.astype(np.float32)[..., 0]
+    # mem = image.astype(np.float32)[..., 1]
+    # im = (im - self.request.image_mean) / self.request.image_stddev
+    # image = np.stack((im, mem), axis=-1)
     if restrictor == self.ALL_MASKED:
       return None, None
 

flexible_hybrid.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import numpy as np
 from google.protobuf import text_format
@@ -21,24 +22,46 @@ def pad_zeros(x, total):
     return x
 
 
+def make_dir(d):
+    """Make directory d if it does not exist."""
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+
+def rdirs(coors, path, its=3):
+    """Recursively make paths."""
+    paths = path.split('/')
+    for idx in reversed(range(its)):
+        if idx == 2:
+            it_path = '/'.join(paths[:-(idx + 1)]) % (pad_zeros(coors[0], 4))
+        elif idx == 1:
+            it_path = '/'.join(paths[:-(idx + 1)]) % (pad_zeros(coors[0], 4), pad_zeros(coors[1], 4))
+        elif idx == 0:
+            it_path = '/'.join(paths[:-(idx + 1)]) % (pad_zeros(coors[0], 4), pad_zeros(coors[1], 4), pad_zeros(coors[2], 4))
+        make_dir(it_path)
+        print 'Made: %s' % it_path
+
+
 # DEFAULTS
 SHAPE = np.array([128, 128, 128])
 CONF = [4992, 16000, 10112]
 PATH_STR = '/local1/dlinsley/connectomics/mag1/x%s/y%s/z%s/110629_k0725_mag1_x%s_y%s_z%s.raw'
-MEM_STR = '/gpfs/data/tserre/data/tmp_ding/mag1/x%s/y%s/z%s/membrane_110629_k0725_mag1_x%s_y%s_z%s.npy'
+MEM_STR = '/gpfs/data/tserre/data/tmp_ding_v2/mag1/x%s/y%s/z%s/membrane_110629_k0725_mag1_x%s_y%s_z%s.npy'
 
 # OPTIONS
 MEMBRANE_MODEL = 'fgru_tmp'  # Allow for dynamic import
 MEMBRANE_CKPT = '/gpfs/data/tserre/data/connectomics/checkpoints/global_2_fb_wide_mini_fb_hgru3d_berson_0_berson_0_2018_10_29_14_58_16_883649/model_63000.ckpt-63000'
-SEED = np.array([15, 15, 17])
 PATH_EXTENT = [3, 3, 3]
 FFN_TRANSPOSE = (0, 1, 2)
 START = [50, 250, 200]
 MEMBRANE_TYPE = 'probability'  # 'threshold'
 
 
-def main(v):
+def main(idx, validate=False, seed='15,15,17'):
     """Apply the FFN routines using fGRUs."""
+    SEED = np.array([int(x) for x in seed.split(',')])
+    rdirs(SEED, MEM_STR)
+    model_shape = (SHAPE * PATH_EXTENT)
     mpath = MEM_STR % (
         pad_zeros(SEED[0], 4),
         pad_zeros(SEED[1], 4),
@@ -46,11 +69,49 @@ def main(v):
         pad_zeros(SEED[0], 4),
         pad_zeros(SEED[1], 4),
         pad_zeros(SEED[2], 4))
-    model_shape = (SHAPE * PATH_EXTENT)
-    if v == 0:
+    if idx == 0:
         # 1. select a volume
-        data = np.load('/gpfs/data/tserre/data/connectomics/datasets/berson_0.npz')
-        vol = data['volume'][:model_shape[0]]
+        if not validate:
+            if np.all(PATH_EXTENT == 1):
+                path = PATH_STR % (
+                    pad_zeros(SEED[0], 4),
+                    pad_zeros(SEED[1], 4),
+                    pad_zeros(SEED[2], 4),
+                    pad_zeros(SEED[0], 4),
+                    pad_zeros(SEED[1], 4),
+                    pad_zeros(SEED[2], 4))
+                vol = np.fromfile(path, dtype='uint8').reshape(SHAPE)
+            else:
+                vol = np.zeros((np.array(SHAPE) * PATH_EXTENT))
+                for z in range(PATH_EXTENT[0]):
+                    for y in range(PATH_EXTENT[1]):
+                        for x in range(PATH_EXTENT[2]):
+                            path = PATH_STR % (
+                                pad_zeros(SEED[0] + x, 4),
+                                pad_zeros(SEED[1] + y, 4),
+                                pad_zeros(SEED[2] + z, 4),
+                                pad_zeros(SEED[0] + x, 4),
+                                pad_zeros(SEED[1] + y, 4),
+                                pad_zeros(SEED[2] + z, 4))
+                            v = np.fromfile(path, dtype='uint8').reshape(SHAPE)
+                            vol[
+                                z * SHAPE[0] : z * SHAPE[0] + SHAPE[0],
+                                y * SHAPE[1] : y * SHAPE[1] + SHAPE[1],
+                                x * SHAPE[2] : x * SHAPE[2] + SHAPE[2]] = v
+        else:
+            data = np.load('/gpfs/data/tserre/data/connectomics/datasets/berson_0.npz')
+            vol = data['volume'][:model_shape[0]]
+            SEED = [99, 99, 99]
+            mpath = MEM_STR % (
+                pad_zeros(SEED[0], 4),
+                pad_zeros(SEED[1], 4),
+                pad_zeros(SEED[2], 4),
+                pad_zeros(SEED[0], 4),
+                pad_zeros(SEED[1], 4),
+                pad_zeros(SEED[2], 4))
+            rdirs(SEED, MEM_STR)
+        print('seed: %s' % SEED)
+        print('mpath: %s' % mpath)
         vol = vol.astype(np.float32) / 255.
 
         # 2. Predict its membranes
@@ -80,7 +141,11 @@ def main(v):
         print 'Saved membrane volume to %s' % mpath
 
     # 4. Start FFN
-    if v == 0:
+    if validate:
+        SEED = [99, 99, 99]
+    seg_dir = 'ding_segmentations/x%s/y%s/z%s/v%s/' % (pad_zeros(SEED[0], 4), pad_zeros(SEED[1], 4), pad_zeros(SEED[2], 4), idx)
+    print 'Saving segmentations to: %s' % seg_dir
+    if idx == 0:
         seed_policy = 'PolicyMembrane'  # 'PolicyPeaks'
     else:
         seed_policy = 'PolicyPeaks'  # 'ShufflePolicyPeaks'
@@ -91,7 +156,7 @@ def main(v):
         model_checkpoint_path: "/gpfs/data/tserre/data/connectomics/checkpoints/feedback_hgru_v5_3l_notemp_f_berson2x_w_memb_r0/model.ckpt-44450"
         model_name: "feedback_hgru_v5_3l_notemp_f.ConvStack3DFFNModel"
         model_args: "{\\"depth\\": 12, \\"fov_size\\": [57, 57, 13], \\"deltas\\": [8, 8, 3]}"
-        segmentation_output_dir: "ding_segmentations/x%s/y%s/z%s/v%s/"
+        segmentation_output_dir: "%s"
         inference_options {
             init_activation: 0.95
             pad_value: 0.05
@@ -99,7 +164,7 @@ def main(v):
             min_boundary_dist { x: 1 y: 1 z: 1}
             segment_threshold: 0.6
             min_segment_size: 4096
-        }''' % (mpath, seed_policy, pad_zeros(SEED[0], 4), pad_zeros(SEED[1], 4), pad_zeros(SEED[2], 4), v)
+        }''' % (mpath, seed_policy, seg_dir)
 
     req = inference_pb2.InferenceRequest()
     _ = text_format.Parse(config, req)
@@ -112,12 +177,16 @@ def main(v):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--v',
-        dest='v',
+        '--idx',
+        dest='idx',
         type=int,
         default=0,
         help='Segmentation version.')
+    parser.add_argument(
+        '--validate',
+        dest='validate',
+        action='store_true',
+        help='Force berson validation dataset.')
     args = parser.parse_args()
     main(**vars(args))
-
 

working_hybrid_script.py
@@ -32,6 +32,7 @@ SEED = np.array([15, 15, 15])
 PATH_EXTENT = [1, 3, 3]
 FFN_TRANSPOSE = (0, 2, 1)
 MEMBRANE_TYPE = 'probability'
+NUM_RESTARTS = 1
 
 # 1. select a volume
 if np.all(PATH_EXTENT == 1):
@@ -98,61 +99,34 @@ mpath = MEM_STR % (
 np.save(mpath, membranes)
 print 'Saved membrane volume to %s' % mpath
 
-# 4. Start FFN V1
-config = '''image {
-  hdf5: "%s"}
-image_mean: 128
-image_stddev: 33
-seed_policy: "PolicyPeaks"
-model_checkpoint_path: "/gpfs/data/tserre/data/connectomics/checkpoints/feedback_hgru_v5_3l_notemp_f_berson2x_w_memb_r0/model.ckpt-44450"
-model_name: "feedback_hgru_v5_3l_notemp_f.ConvStack3DFFNModel"
-model_args: "{\\"depth\\": 12, \\"fov_size\\": [57, 57, 13], \\"deltas\\": [8, 8, 3]}" 
-segmentation_output_dir: "ding_segmentations/x%s/y%s/z%s/v1"
-inference_options {
-  init_activation: 0.95
-  pad_value: 0.05
-  move_threshold: 0.7
-  min_boundary_dist { x: 1 y: 1 z: 1}
-  segment_threshold: 0.5
-  min_segment_size: 4096
-}''' % (mpath, pad_zeros(SEED[0], 4), pad_zeros(SEED[1], 4), pad_zeros(SEED[2], 4))
+# 4. Start FFN
+for idx in range(NUM_RESTARTS):
+    if idx == 0:
+        seed_policy = 'PolicyPeaks'
+    else:
+        seed_policy = 'ShufflePolicyPeaks'
+    config = '''image {hdf5: "%s"}
+        image_mean: 128
+        image_stddev: 33
+        seed_policy: "%s"
+        model_checkpoint_path: "/gpfs/data/tserre/data/connectomics/checkpoints/feedback_hgru_v5_3l_notemp_f_berson2x_w_memb_r0/model.ckpt-44450"
+        model_name: "feedback_hgru_v5_3l_notemp_f.ConvStack3DFFNModel"
+        model_args: "{\\"depth\\": 12, \\"fov_size\\": [57, 57, 13], \\"deltas\\": [8, 8, 3]}"
+        segmentation_output_dir: "ding_segmentations/x%s/y%s/z%s/v%s/"
+        inference_options {
+            init_activation: 0.95
+            pad_value: 0.05
+            move_threshold: 0.6  # Used to be 0.7
+            min_boundary_dist { x: 1 y: 1 z: 1}
+            segment_threshold: 0.6
+            min_segment_size: 4096
+        }''' % (mpath, seed_policy, pad_zeros(SEED[0], 4), pad_zeros(SEED[1], 4), pad_zeros(SEED[2], 4), idx)
 
-req = inference_pb2.InferenceRequest()
-_ = text_format.Parse(config, req)
-runner = inference.Runner()
-runner.start(req, tag='_inference')
-runner.run((0, 0, 0),
-    (model_shape[0], model_shape[1], model_shape[2]))
-
-# 5. Start FFN V2
-config = '''image {
-  hdf5: "%s"}
-image_mean: 128
-image_stddev: 33
-seed_policy: "PolicyPeaks"
-model_checkpoint_path: "/gpfs/data/tserre/data/connectomics/checkpoints/feedback_hgru_v5_3l_notemp_f_berson2x_w_memb_r0/model.ckpt-44450"
-model_name: "feedback_hgru_v5_3l_notemp_f.ConvStack3DFFNModel"
-model_args: "{\\"depth\\": 12, \\"fov_size\\": [57, 57, 13], \\"deltas\\": [8, 8, 3]}"
-segmentation_output_dir: "ding_segmentations/x%s/y%s/z%s/v2"
-inference_options {
-  init_activation: 0.95
-  pad_value: 0.05
-  move_threshold: 0.7
-  min_boundary_dist { x: 1 y: 1 z: 1}
-  segment_threshold: 0.5
-  min_segment_size: 4096
-}''' % (mpath, pad_zeros(SEED[0], 4), pad_zeros(SEED[1], 4), pad_zeros(SEED[2], 4))
-
-req = inference_pb2.InferenceRequest()
-_ = text_format.Parse(config, req)
-runner = inference.Runner()
-runner.start(req, tag='_inference')
-runner.run((0, 0, 0),
-    (model_shape[0], model_shape[1], model_shape[2]))
-
-
-# canvas, alignment = runner.make_canvas((0, 0, 0), model_shape)
-# canvas.segment_at((60, 60, 60))
-#                   dynamic_image=inference.DynamicImage(),
-#                   vis_update_every=1)
+    req = inference_pb2.InferenceRequest()
+    _ = text_format.Parse(config, req)
+    if idx == 0:
+        runner = inference.Runner()
+    runner.start(req, tag='_inference')
+    runner.run((0, 0, 0),
+        (model_shape[0], model_shape[1], model_shape[2]))
 