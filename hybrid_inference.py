bded54635668393064fd358b0bddd82210773503
Drew Linsley <drewlinsley@gmail.com>
2019-12-06 18:10:08 -0500

    Working on membrane augmentations.

--- hybrid_inference.py
+++ hybrid_inference.py
@@ -10,58 +10,15 @@ from google.protobuf import text_format
 from ffn.inference import inference
 from ffn.inference import inference_pb2
 from membrane.models import l3_fgru_constr as fgru
+from utils.hybrid_utils import recursive_make_dir
+from utils.hybrid_utils import pad_zeros
+from utils.hybrid_utils import _bump_logit_map
+from utils.hybrid_utils import rdirs
+from scipy.special import expit
 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-
-def recursive_make_dir(path, s=3):
-    """Recursively build output paths."""
-    split_path = path.split(os.path.sep)
-    for idx, p in enumerate(split_path):
-        if idx > s:
-            d = '/'.join(split_path[:idx])
-            if not os.path.exists(d):
-                os.makedirs(d)
-                # print('Created: %s' % d)
-            else:
-                # print('Reusing: %s' % d)
-                pass
-
-
-def pad_zeros(x, total):
-    """Pad x with zeros to total digits."""
-    if not isinstance(x, basestring):
-        x = str(x)
-    total = total - len(x)
-    for idx in range(total):
-        x = '0' + x
-    return x
-
-
-def make_dir(d):
-    """Make directory d if it does not exist."""
-    if not os.path.exists(d):
-        os.makedirs(d)
-
-
-def rdirs(coors, path, its=3):
-    """Recursively make paths."""
-    paths = path.split('/')
-    for idx in reversed(range(its)):
-        if idx == 2:
-            it_path = '/'.join(paths[:-(idx + 1)]) % (pad_zeros(coors[0], 4))
-        elif idx == 1:
-            it_path = '/'.join(paths[:-(idx + 1)]) % (
-                pad_zeros(coors[0], 4), pad_zeros(coors[1], 4))
-        elif idx == 0:
-            it_path = '/'.join(paths[:-(idx + 1)]) % (
-                pad_zeros(coors[0], 4),
-                pad_zeros(coors[1], 4),
-                pad_zeros(coors[2], 4))
-        make_dir(it_path)
-        print 'Made: %s' % it_path
 
 
 def get_segmentation(
@@ -85,8 +42,10 @@ def get_segmentation(
         seed_policy='PolicyMembraneExtra',  # 'PolicyPeaks'
         downsize=False,
         membrane_slice=[64, 576, 576],
+        membrane_overlap_factor=2,
         debug_resize=False,
         debug_nii=False,
+        test_time_augs=['lr_flip', 'ud_flip', 'depth_flip', 'rot90', 'rot180', 'rot270'],  # noqa
         path_extent=None,  # [1, 1, 1],
         rotate=False):
     """Apply the FFN routines using fGRUs."""
@@ -99,6 +58,8 @@ def get_segmentation(
         path_extent = np.array([int(s) for s in path_extent.split(',')])
     if isinstance(membrane_slice, str):
         membrane_slice = [int(s) for s in membrane_slice.split(',')]
+    if len(membrane_slice) != 3:
+        raise RuntimeError('membrane_slice needs to be a len(3) list.')
     assert move_threshold is not None
     assert segment_threshold is not None
     rdirs(seed, config.mem_str)
@@ -153,13 +114,13 @@ def get_segmentation(
                 pad_zeros(seed[2], 4))
             rdirs(seed, config.mem_str)
         vol = vol.astype(np.float32) / 255.
-        vol_ = vol.shape
+        _vol = vol.shape
         print('seed: %s' % seed)
         print('mpath: %s' % mpath)
         print('volume size: (%s, %s, %s)' % (
-            vol_[0],
-            vol_[1],
-            vol_[2]))
+            _vol[0],
+            _vol[1],
+            _vol[2]))
 
         # 2. Predict its membranes
         if downsize > 1:
@@ -174,23 +135,56 @@ def get_segmentation(
             membrane_model_shape = model_shape
             model_shape = (config.shape * path_extent)
         if membrane_slice is not None:
-            assert isinstance(membrane_slice, list), 'Make membrane_slice a list.'
+            assert isinstance(
+                membrane_slice, list), 'Make membrane_slice a list.'
             # Split up membrane along z-axis into k-voxel chunks
-            z_splits = np.arange(0, model_shape[0], membrane_slice[0])
-            y_splits = np.arange(0, model_shape[1], membrane_slice[1])
-            x_splits = np.arange(0, model_shape[2], membrane_slice[2])
+            # Include an overlap so that you have 1 extra slice per dim
+            adj_membrane_slice = np.array(
+                membrane_slice) / membrane_overlap_factor
+            z_splits = np.arange(0, model_shape[0], adj_membrane_slice[0])
+            y_splits = np.arange(0, model_shape[1], adj_membrane_slice[1])
+            x_splits = np.arange(0, model_shape[2], adj_membrane_slice[2])
             vols = []
             for z_idx in z_splits:
                 for y_idx in y_splits:
                     for x_idx in x_splits:
+                        if z_idx == 0:
+                            zu = z_idx
+                            zo = z_idx + membrane_slice[0]
+                        else:
+                            zu = z_idx - adj_membrane_slice[0]
+                            zo = zu + membrane_slice[0]
+                        if y_idx == 0:
+                            yu = y_idx
+                            yo = y_idx + membrane_slice[0]
+                        else:
+                            yu = y_idx - adj_membrane_slice[0]
+                            yo = yu + membrane_slice[0]
+                        if x_idx == 0:
+                            xu = x_idx
+                            xo = x_idx + membrane_slice[0]
+                        else:
+                            xu = x_idx - adj_membrane_slice[0]
+                            xo = xu + membrane_slice[0]
                         vols += [vol[
-                            z_idx: z_idx + membrane_slice[0],
-                            y_idx: y_idx + membrane_slice[1],
-                            x_idx: x_idx + membrane_slice[2]]]
+                            zu: zo,
+                            yu: yo,
+                            xu: xo]]
             original_vol = np.copy(vol)
-            vol = np.stack(vols)
+            try:
+                vol = np.stack(vols)
+            except Exception:
+                print(
+                    'Mismatch in volume_size/membrane slicing {}'.format(
+                        [vs.shape for vs in vols]))
+                os._exit(1)
             del vols  # Garbage collect
             membrane_model_shape = membrane_slice
+        print membrane_model_shape
+        print vol.shape
+        if test_time_augs is not None:
+            for aug in test_time_augs:
+                pass
         membranes = fgru.main(
             test=vol,
             evaluate=True,
@@ -202,65 +196,94 @@ def get_segmentation(
                 membrane_model_shape, [3])).tolist(),
             checkpoint=config.membrane_ckpt)
         if membrane_slice is not None:
-            # reconstruct
-            membrane_model_shape = tuple(list(vol_) + [3])
-            rmembranes = np.zeros(membrane_model_shape, dtype=np.float32)
+            # Reconstruct, accounting for overlap
+            # membrane_model_shape = tuple(list(_vol) + [3])
+            rmembranes = np.zeros(_vol, dtype=np.float32)
+            normalization = np.zeros_like(rmembranes)
             count = 0
+            vols = []
+            bump_map = _bump_logit_map(membrane_slice)
+            bump_map = 1 - bump_map / bump_map.min()
             for z_idx in z_splits:
                 for y_idx in y_splits:
                     for x_idx in x_splits:
+                        if z_idx == 0:
+                            zu = z_idx
+                            zo = z_idx + membrane_slice[0]
+                        else:
+                            zu = z_idx - adj_membrane_slice[0]
+                            zo = zu + membrane_slice[0]
+                        if y_idx == 0:
+                            yu = y_idx
+                            yo = y_idx + membrane_slice[0]
+                        else:
+                            yu = y_idx - adj_membrane_slice[0]
+                            yo = yu + membrane_slice[0]
+                        if x_idx == 0:
+                            xu = x_idx
+                            xo = x_idx + membrane_slice[0]
+                        else:
+                            xu = x_idx - adj_membrane_slice[0]
+                            xo = xu + membrane_slice[0]
                         rmembranes[
-                            z_idx: z_idx + membrane_slice[0],
-                            y_idx: y_idx + membrane_slice[1],
-                            x_idx: x_idx + membrane_slice[2]] = membranes[count]
+                            zu: zo,
+                            yu: yo,
+                            xu: xo] += membranes[count].squeeze(
+                                0).max(-1) * bump_map
+                        normalization[
+                            zu: zo,
+                            yu: yo,
+                            xu: xo] += bump_map  # 1.
                         count += 1
+            import ipdb;ipdb.set_trace()
+            rmembranes /= normalization
             membranes = rmembranes[None]
             vol = original_vol
 
         # 3. Concat the volume w/ membranes and pass to FFN
-        if membrane_type == 'probability':
-            print 'Membrane: %s' % membrane_type
-            proc_membrane = (
-                membranes[0, :, :, :, :3].mean(-1)).transpose(ffn_transpose)
-        elif membrane_type == 'threshold':
-            print 'Membrane: %s' % membrane_type
-            proc_membrane = (
-                membranes[0, :, :, :, :3].mean(-1) > 0.5).astype(
-                    membranes.dtype).transpose(ffn_transpose)
-        else:
-            raise NotImplementedError
-        if downsize > 1:
-            if debug_resize:
-                cmembranes = deepcopy(proc_membrane)
-            # Upsample
-            proc_membrane = transform.rescale(
-                proc_membrane,
-                scale=downsize,
-                order=1,
-                mode='constant',
-                multichannel=False,
-                preserve_range=True,
-                anti_aliasing=True)
-            if debug_resize:
-                from matplotlib import pyplot as plt
-                plt.subplot(141)
-                plt.imshow(cvol[50])
-                plt.subplot(142)
-                plt.imshow(proc_membrane[50])
-                plt.subplot(143)
-                plt.imshow(vol[25])
-                plt.subplot(144)
-                plt.imshow(cmembranes[25])
-                plt.show()
+        # if membrane_type == 'probability':
+        #     print 'Membrane: %s' % membrane_type
+        #     proc_membrane = (
+        #         membranes[0, :, :, :, :3].mean(-1)).transpose(ffn_transpose)
+        # elif membrane_type == 'threshold':
+        #     print 'Membrane: %s' % membrane_type
+        #     proc_membrane = (
+        #         membranes[0, :, :, :, :3].mean(-1) > 0.5).astype(
+        #             membranes.dtype).transpose(ffn_transpose)
+        # else:
+        #     raise NotImplementedError
+        # if downsize > 1:
+        #     if debug_resize:
+        #         cmembranes = deepcopy(proc_membrane)
+        #     # Upsample
+        #     proc_membrane = transform.rescale(
+        #         proc_membrane,
+        #         scale=downsize,
+        #         order=1,
+        #         mode='constant',
+        #         multichannel=False,
+        #         preserve_range=True,
+        #         anti_aliasing=True)
+        #     if debug_resize:
+        #         from matplotlib import pyplot as plt
+        #         plt.subplot(141)
+        #         plt.imshow(cvol[50])
+        #         plt.subplot(142)
+        #         plt.imshow(proc_membrane[50])
+        #         plt.subplot(143)
+        #         plt.imshow(vol[25])
+        #         plt.subplot(144)
+        #         plt.imshow(cmembranes[25])
+        #         plt.show()
 
         vol = vol.transpose(ffn_transpose)  # ).astype(np.uint8)
         membranes = np.stack(
-            (vol, proc_membrane), axis=-1).astype(np.float32) * 255.
+            (vol, membranes), axis=-1).astype(np.float32) * 255.
         if rotate:
             membranes = np.rot90(membranes, k=1, axes=(1, 2))
         np.save(mpath, membranes)
         print 'Saved membrane volume to %s' % mpath
-        del membranes, proc_membrane, vol  # Garbage collect
+        del membranes, vol, bump_map  # Garbage collect
 
     mpath = '%s.npy' % mpath
 
@@ -470,4 +493,3 @@ if __name__ == '__main__':
     get_segmentation(**vars(args))
     end = time.time()
     print('Segmentation took {}'.format(end - start))
-
